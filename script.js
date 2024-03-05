928baebb798115b5e76a94f782dcd3232a98613c
Mike <mw@Mikes-MacBook-Pro.local>
2024-03-05 14:03:09 -0800

    Implement project objects and task assignment.
    What changed and why:
    Created CreateProject, newProjectHandler, currentProjectIdentity. Brought functions inside the interfaceController scope.
    
    Notes:
    currentProjectIdentifier is hardcoded to home
    
    TODO:
    build project display links and update currentProjectIdentifier.

--- script.js
+++ script.js
@@ -6,42 +6,25 @@ function CreateTask ({ newPriority, newTitle, newDate, taskNumber}) {
     this.name = newTitle;
     this.date = newDate;
     this.number = taskNumber;
-    
-    
-    
 };
 
-function newTaskHandler () {
+function CreateProject (name) {
+    console.log(`new project factory started ${name}`)
+    this.name = name;
+    this.tasks = [];
 
-    let newPriority = document.getElementById('priority').value;
-
-    let newTitle = document.getElementById('input-task-title').value;
-
-    let newDate = document.getElementById('input-task-due-date').value;
-
-
-    
-    if (newPriority !== "" && newTitle !== "" && newDate !== "") {
-
-    console.log(`ready to start task factory  ${newPriority}, ${newTitle}, ${newDate}, ${taskNumber}`);
-
-        let task = new CreateTask({newPriority, newTitle, newDate, taskNumber});
-        clearFields();
+};
 
 
-        console.log(task);
-
-        taskNumber += 1;
-        
-
-    } else {
-        console.log('No task created... undefined values in input!');
-    }
-}
 
 interfaceController();
 
+
 function interfaceController() {
+
+    let home = new CreateProject('home');
+    console.log ({home});
+    currentProject = home;
 
     let confirm = document.getElementById('confirm');
     confirm.addEventListener('click', newTaskHandler);
@@ -49,13 +32,74 @@ function interfaceController() {
     let reset = document.getElementById('reset');
     reset.addEventListener('click', clearFields);
 
+    let addProject = document.getElementById('add-project');
+    addProject.addEventListener('click', newProjectHandler);
+
+
+    function clearFields () {
+        document.getElementById('priority').selectedIndex = 0;
+        document.getElementById('input-task-title').value ='';
+        document.getElementById('input-task-due-date').value = '';
+    }
+
+    function newProjectHandler () {
+        let newProject = document.getElementById('input-project-title').value;
+        let project = new CreateProject(newProject);
+        console.log( {project} );
+    
+    }
+
+    function currentProjectIdentity (home) {
+        console.log(`the current project is ${home}`)
+        return home;
+    }
+    
+
+
+    function newTaskHandler () {
+
+        let currentProject = currentProjectIdentity();
+    
+        let newPriority = document.getElementById('priority').value;
+    
+        let newTitle = document.getElementById('input-task-title').value;
+    
+        let newDate = document.getElementById('input-task-due-date').value;
+    
+    
+        
+        if (newPriority !== "" && newTitle !== "" && newDate !== "") {
+    
+        console.log(`ready to start task factory  ${newPriority}, ${newTitle}, ${newDate}, ${taskNumber}`);
+    
+            let task = new CreateTask({newPriority, newTitle, newDate, taskNumber});
+            home.tasks.push(task);
+            clearFields();
+    
+    
+            console.log(task);
+            console.log({currentProject});
+            console.log( {home});
+    
+            taskNumber += 1;
+            
+    
+        } else {
+            console.log('No task created... undefined values in input!');
+        }
+    }
+    
+   
+    
+
+
+
+
+
+
+
 }
 
-function clearFields () {
-    document.getElementById('priority').selectedIndex = 0;
-    document.getElementById('input-task-title').value ='';
-    document.getElementById('input-task-due-date').value = '';
-}
 
 function displayProject () {
 
@@ -70,25 +114,15 @@ function displayNewTask() {
 }
 
 
-function newProjectHandler () {
-
-}
-
-function currentProjectIdentity () {
-
-}
 
 
 
 
 
-function createProject (name) {
 
-}
 
-function assignTask () {
 
-}
+
 
 function projectConstructor () {
 
